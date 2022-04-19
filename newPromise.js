0be63bf440dd2c3110cbd675a35aa91c72901984
njsaugat <njsaugat@gmail.com>
2022-04-19 15:57:14 +0545

    async await learning

--- newPromise.js
+++ newPromise.js
@@ -1,25 +1,49 @@
-
+// prom //running a promise before declaring it wont run it
+// .then(fun1)
+// .then(fun1)
+// .then(fun1)
+// .catch(fun2)
 function changePromise(resolve,reject){
     const a=1+1;
     if(a===2){
-        resolve('success');
+        resolve(()=>('success'));
+        // resolve('success');
     }else{
         reject('failed');
     }
 }
+// function changePromise(resolve,reject){
+//     setTimeout(()=>{
+//         const a=1+1;
+//         if(a===2){
+//             resolve('success');
+//         }else{
+//             reject('failed');
+//         }
+//     },5000)
+    
+// }
+
 
 const prom=new Promise(changePromise)
 
 function fun1(message){
-    console.log('this is in the '+ message)
+    console.log('this is in the '+ message())
 
 }
 
 function fun2(message){
     console.log('this is in the '+ message)
 }
+// prom
+// .then(fun1,fun1,fun1)
+// // .then(fun1)
+// // .then(fun1)
+// .catch(fun2)
 prom
 .then(fun1)
-.then(fun1)
-.then(fun1)
+// .then(fun1)
+// .then(fun1)
 .catch(fun2)
+
+console.log("hello world")
---
+++ obj.js
@@ -0,0 +1,17 @@
+const person={
+    name:'ram',
+    sayMyName:()=>{
+        return function hello(){
+            // name='Ram';
+            console.log(`My name is ${this.name}`);
+        }
+    },
+    sayMyName2:()=> {
+        console.log(`My name is ${this.name}`)
+
+    }
+}
+
+// person.sayMyName()()
+person.sayMyName2()
+// console.log(person.sayMyName())
