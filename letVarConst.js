51d8714516ab80a3f7fbff830c5491d0ef311bb2
njsaugat <njsaugat@gmail.com>
2022-04-27 15:07:35 +0545

    let var and const

---
+++ letVarConst.js
@@ -0,0 +1,30 @@
+let i=1
+console.log(++i);
+const a=2
+// a=34;
+console.log(a);
+// error when modifying the vlaue of const
+// const is like a rock
+// it's value wont get changed and modified
+// just like a  rock
+
+// let is like a mini version
+// the vlaue of let can be changed, also type can be change
+//but like redeclaring gives error
+
+// valid with let:
+let b=5;
+b=543;
+console.log(b)// value changed
+b='hello '
+console.log(b);
+
+
+//invalid with let
+let c=5;
+// let c=75;
+//      ^
+//      |
+//      |
+// so this is not allowed with let]
+// ie no redeclaration
