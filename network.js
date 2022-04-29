bc150295c5ded038c6104ff8070b210bae036880
njsaugat <njsaugat@gmail.com>
2022-04-29 13:18:34 +0545

    playing with http and networks

---
+++ network.js
@@ -0,0 +1,18 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    if (req.url === '/favicon.ico') {// writing this line would ensure we dont have printed twice
+        res.end();
+        return;
+    }
+    console.log('hello there from console log');
+    for(let i=0;i<1000;i++){
+        res.write('hello there');
+    }
+    res.end();
+    // console.log(res.end().constructor.name);
+
+    console.log('heyy there!!')
+});
+server.listen(3000,()=>console.log('hello'));// this is async
+
+console.log('hello;I will be printed first IG');// this will run first
