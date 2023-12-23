f74ca0c85fba0131aa65b6fb3a0424e0ccdeecf9
Saugat Poudel <88492692+njsaugat@users.noreply.github.com>
2023-12-23 20:43:44 +0545

    remove comment

--- findCir.js
+++ findCir.js
@@ -1,13 +1,12 @@
 // const circle=require('./circle.js');// esari circle access garo vane circle is object ani object auxa
 // // console.log(calcArea(1));
 // console.log(circle.calcArea(circle.rad));// esari use garda jhur hunxa so we use destructuring
-// console.log(circle.rad);// 
+// console.log(circle.rad);//
 
 // const {calcArea,rad}=require('./circle.js')
 // console.log(calcArea(rad));
 
 // const {personObj}=require('./circle.js')
 // console.log(personObj);
-// find cir
-import add from './circle.mjs'
-console.log(add(4,4));
+import add from "./circle.mjs";
+console.log(add(4, 4));
