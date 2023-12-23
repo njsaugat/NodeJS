2afbd69a94f2999cad5f00e6ebeba99443fbc605
Saugat Poudel <88492692+njsaugat@users.noreply.github.com>
2023-12-23 20:44:26 +0545

    http

--- http.js
+++ http.js
@@ -2,25 +2,24 @@
 // console.log(http.createServer.toString())
 // const hello=http.Server.toString();
 // console.log(hello);
-// find http
-const http=require('http');
+const http = require("http");
 
-const server=http.createServer((request,response)=>{
-    // console.log(response)
-    // so like request auxa ani tesko url lai compare garne ho nita
-    console.log(request.url)
-    // so like based on different request, we can send 
-    // different websites
-    // if request.url==='/about' ==> then send about page and so on
-    if( request.url==='/about'){
-        response.end('know about us');
-    }
-    if( request.url==='/'){
-        response.end('hello there');
-    }
-    response.end('OOPS!')
-    // response.write(``);
-    // response.end();
-})
+const server = http.createServer((request, response) => {
+  // console.log(response)
+  // so like request auxa ani tesko url lai compare garne ho nita
+  console.log(request.url);
+  // so like based on different request, we can send
+  // different websites
+  // if request.url==='/about' ==> then send about page and so on
+  if (request.url === "/about") {
+    response.end("know about us");
+  }
+  if (request.url === "/") {
+    response.end("hello there");
+  }
+  response.end("OOPS!");
+  // response.write(``);
+  // response.end();
+});
 
-server.listen(3000)
+server.listen(3000);
