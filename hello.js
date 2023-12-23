08672b5e8ef674ed48ea204c6ffa946b4fcd0c55
Saugat Poudel <88492692+njsaugat@users.noreply.github.com>
2023-12-23 20:44:12 +0545

    remove comment

--- hello.js
+++ hello.js
@@ -1,17 +1,16 @@
-// find hello
-const prom=new Promise((resolve,reject)=>{
-    const a=4;
-    if(a==4){
-        resolve('true')
-    }else{
-        reject('false')
-    }
-})
+const prom = new Promise((resolve, reject) => {
+  const a = 4;
+  if (a == 4) {
+    resolve("true");
+  } else {
+    reject("false");
+  }
+});
 
 prom
-.then((mess)=>{
-    console.log('hello world '+ mess)
-})
-.catch((mess)=>{
-    console.log('not hello world '+ mess)
-})
+  .then((mess) => {
+    console.log("hello world " + mess);
+  })
+  .catch((mess) => {
+    console.log("not hello world " + mess);
+  });
